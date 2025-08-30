e8653a218877db9633fb426addb409434f6a2e4f
bennjung <jss4396@gmail.com>
2025-08-30 21:54:03 +0900

    feat: Implement complete MiniAMM functionality

--- 1a/src/MiniAMM.sol
+++ 1a/src/MiniAMM.sol
@@ -14,29 +14,96 @@ contract MiniAMM is IMiniAMM, IMiniAMMEvents {
     address public tokenX;
     address public tokenY;
 
-    // implement constructor
-    constructor(address _tokenX, address _tokenY) {}
+    // implement constructor 
+    constructor(address _tokenX, address _tokenY) {
+        require(_tokenX != address(0), "tokenX cannot be zero address");
+        require(_tokenY != address(0), "tokenY cannot be zero address");
+        if (_tokenX == _tokenY) {
+            revert("Tokens must be different");
+        }
+
+        if (_tokenX < _tokenY) {
+            tokenX = _tokenX;
+            tokenY = _tokenY;
+        } else {
+            tokenX = _tokenY;
+            tokenY = _tokenX;
+        }
+    }
 
     // add parameters and implement function.
     // this function will determine the initial 'k'.
-    function _addLiquidityFirstTime() internal {}
+    function _addLiquidityFirstTime(uint256 xAmount, uint256 yAmount) internal {
+        require(xAmount > 0, "Amounts must be greater than 0");
+        require(yAmount > 0, "Amounts must be greater than 0");
+        IERC20(tokenX).transferFrom(msg.sender, address(this), xAmount);
+        IERC20(tokenY).transferFrom(msg.sender, address(this), yAmount);
+        xReserve = xAmount;
+        yReserve = yAmount;
+        k = xReserve * yReserve;
+
+        emit AddLiquidity(xAmount, yAmount);
+        
+    }
 
     // add parameters and implement function.
     // this function will increase the 'k'
     // because it is transferring liquidity from users to this contract.
-    function _addLiquidityNotFirstTime() internal {}
+    function _addLiquidityNotFirstTime(uint256 xAmount, uint256 yAmount) internal {
+        require(xAmount > 0, "tokenX amount cannot be zero");
+        require(yAmount > 0, "tokenY amount cannot be zero");
+        require(xAmount * yReserve == yAmount * xReserve, "Invaild ratio") ;
+
+        IERC20(tokenX).transferFrom(msg.sender, address(this), xAmount);
+        IERC20(tokenY).transferFrom(msg.sender, address(this), yAmount);
+        xReserve += xAmount;
+        yReserve += yAmount;
+        k = xReserve * yReserve;
+
+        emit AddLiquidity(xAmount, yAmount);
+
+    }
 
     // complete the function
     function addLiquidity(uint256 xAmountIn, uint256 yAmountIn) external {
         if (k == 0) {
             // add params
-            _addLiquidityFirstTime();
+            _addLiquidityFirstTime(xAmountIn, yAmountIn);
         } else {
             // add params
-            _addLiquidityNotFirstTime();
+            _addLiquidityNotFirstTime(xAmountIn, yAmountIn);
         }
     }
 
     // complete the function
-    function swap(uint256 xAmountIn, uint256 yAmountIn) external {}
+    function swap(uint256 xAmountIn, uint256 yAmountIn) external {
+        uint256 outputAmt = 0;
+        require(k > 0, "No liquidity in pool");
+        require(xAmountIn > 0 || yAmountIn > 0, "Must swap at least one token");
+        require(xReserve >= xAmountIn && yReserve >= yAmountIn, "Insufficient liquidity" );
+        
+        
+        // tokenx -> tokeny
+        if (xAmountIn > 0 && yAmountIn == 0) {
+            IERC20(tokenX).transferFrom(msg.sender, address(this), xAmountIn);
+            outputAmt = yReserve - (k / (xReserve + xAmountIn) );
+            IERC20(tokenY).transfer(msg.sender, outputAmt);
+            xReserve += xAmountIn;
+            yReserve -= outputAmt;
+            emit Swap(xAmountIn, outputAmt);
+        // tokeny -> tokenx
+        } else if (yAmountIn > 0 && xAmountIn == 0) {
+            IERC20(tokenY).transferFrom(msg.sender, address(this), yAmountIn);
+            outputAmt = xReserve - (k / (yReserve + yAmountIn) );
+            IERC20(tokenX).transfer(msg.sender, outputAmt);
+            yReserve += yAmountIn;
+            xReserve -= outputAmt;
+            emit Swap(yAmountIn, outputAmt);
+        } else {
+            revert("Can only swap one direction at a time");
+        }
+
+        
+
+    }
 }
